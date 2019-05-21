edb04fe8184879ff4f0963cce613d0a4f94b3cae
Sunil Patel <sp@therealdeal.com>
2019-05-21 16:29:58 -0400

    updated assets file version.

--- Core/Init.php
+++ Core/Init.php
@@ -27,7 +27,7 @@ class Init
       'trd-core-style',
       TRD_CORE_URL.'/assets/css/trd-core.min.css',
       false,
-      '1.1.0'
+      filemtime( TRD_CORE_DIR.'/assets/css/trd-core.min.css' )
     );
   }
 
@@ -38,7 +38,7 @@ class Init
       'trd-core-script',
       TRD_CORE_URL.'/assets/js/trd-core.min.js',
       array('jquery'),
-      '1.0.0',
+      filemtime( TRD_CORE_DIR.'/assets/css/trd-core.min.js' ),
       true
     );
     wp_localize_script(
