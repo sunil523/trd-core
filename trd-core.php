4fe1b1e2382fba9058b5019df992d7f1ca9f2a49
Sunil Patel <sp@therealdeal.com>
2019-06-18 16:16:47 -0400

    move the common tags to the plugin.

--- trd-core.php
+++ trd-core.php
@@ -14,12 +14,17 @@ define('TRD_CORE_URL', plugin_dir_url( __FILE__ ) );
 
 // autoload the class from the plugin
 spl_autoload_register(function($className){
-    $className = str_replace('TRD\\', '', $className);
-    $className = str_replace('\\', '/', $className);
-    $filePath = plugin_dir_path(__FILE__).$className.'.php';
-    if(file_exists($filePath)) require_once $filePath;
+  $className = str_replace('TRD\\', '', $className);
+  $className = str_replace('\\', '/', $className);
+  $filePath = plugin_dir_path(__FILE__).$className.'.php';
+  if(file_exists($filePath)) require_once $filePath;
 });
-
+/** Remove this when wordpress is upgrated to 5.2 */
+if ( ! function_exists( 'wp_body_open' ) ) {
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
 new \TRD\Core\Init();
 new \TRD\Widgets\Init();
 new \TRD\ShortCode\Init();
