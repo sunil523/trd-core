a495de011eeed2708c7a593e732b2d85f275543b
Sunil Patel <sp@therealdeal.com>
2019-06-24 10:15:31 -0400

    working to add new footer.

--- View/footer.php
+++ View/footer.php
@@ -1,15 +1,16 @@
 <footer class="trd-footer">
   <div class="trd-footer-container">
-    <div class="row">
-    <?php
-      wp_nav_menu( array(
-        'menu_class' => 'trd-nav-regions',
-        'container'  => '',
-        'items_wrap' => '<section id="%1$s" class="%2$s">%3$s</section>',
-        'walker'     => new \TRD\Core\WP\Nav_Walker(),
-        'location'   => 'footer_primary',
-      ) );
-    ?>
-    </div>
+    <section class="trd-footer-links">
+      <a href="/about-us/">About Us</a>
+      <a href="/contact-us/">Contact Us</a>
+      <a href="/privacy-policy/">Privacy Policy</a>
+      <a href="/store/">Shop</a>
+      <a href="/real-estate-professional-directory/">Directory</a>
+    </section>
+    <section class="trd-footer-copyright">
+      <p>All rights reserved &copy; <?php echo date('Y');?> <span>The Real Deal</span> is a registered Trademark of Korangy Publishing Inc.</p>
+      <address>450 West 31st Street, New York, NY10001</address>
+      <p>Phone: 212-260-1332</p>
+    </section>
   </div>
 </footer>
